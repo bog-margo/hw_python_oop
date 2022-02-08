a97daf275847d32bb728497d9b033d1736638c0f
Margarita <margeritis@gmail.com>
2022-02-08 16:20:52 +0300

    Corrections after review

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
-from typing import Dict, Type, Union
 from dataclasses import dataclass
+from typing import Dict
 
 
 @dataclass
@@ -55,11 +55,11 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info_message = InfoMessage(self.__class__.__name__,
-                                   self.duration,
-                                   self.get_distance(),
-                                   self.get_mean_speed(),
-                                   self.get_spent_calories())
+        info_message: str = InfoMessage(self.__class__.__name__,
+                                        self.duration,
+                                        self.get_distance(),
+                                        self.get_mean_speed(),
+                                        self.get_spent_calories())
         return info_message
 
 
@@ -84,8 +84,7 @@ class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
     CALORIES_MEAN_MULTIPLICATION_WEIGHT: float = 0.035
-    CALORIES_MEAN_MULTIPLICATION_SPEED: int = 2
-    CALORIES_MEAN_MULTIPLICATION_SOMETHING_ELSE: float = 0.029
+    CALORIES_MEAN_MULTIPLICATION_COEFF: float = 0.029
 
     def __init__(self,
                  action: int,
@@ -102,11 +101,10 @@ class SportsWalking(Training):
             self.CALORIES_MEAN_MULTIPLICATION_WEIGHT
             * self.weight)
         calories_2: float = (
-            self.get_mean_speed()
-            ** self.CALORIES_MEAN_MULTIPLICATION_SPEED // self.height)
+            self.get_mean_speed() ** 2 // self.height)
         calories_3: float = (
             calories_2
-            * self.CALORIES_MEAN_MULTIPLICATION_SOMETHING_ELSE * self.weight)
+            * self.CALORIES_MEAN_MULTIPLICATION_COEFF * self.weight)
         calories: float = (
             (calories_1 + calories_3) * self.duration
             * self.HOURS_TO_MIN)
@@ -150,10 +148,10 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    type_dict: Dict[str, Type[Union[Swimming, Running, SportsWalking]]] = (
+    type_dict: Dict[str, Training] = (
         {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking})
     if workout_type not in type_dict:
-        raise NotImplementedError('Неизвестный тип тренировки')
+        raise ValueError('Неизвестный тип тренировки')
     return type_dict[workout_type](*data)
 
 
